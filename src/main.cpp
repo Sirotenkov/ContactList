f03e33398991594893f026fe15f92fe29338b000
Алексей <aleksej.sirotenkov@simaster.ru>
2024-09-25 22:33:14 +0300

    Изменен main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-//#include <QMetaType>
 #include <QtQuick>
 #include <QStandardPaths>
+#include <QQmlEngine>
 
 #include <auroraapp.h>
 
-#include "models/contactmodel.h"
-#include "models/contactsdatabasemodel.h"
 #include "viewmodels/contactviewmodel.h"
-#include <viewmodels/abonentcardviewmodel.h>
-//#include <models/contactmodel.h>
-//#include <models/contactsdatabasemodel.h>
-//#include <viewmodels/contactviewmodel.h>
+#include "viewmodels/abonentcardviewmodel.h"
+
+#include "dto/databasestruct.h"
+#include "dao/databasecontacts.h"
 
 int main(int argc, char *argv[])
 {
@@ -20,18 +18,17 @@ int main(int argc, char *argv[])
     application->setOrganizationName(QStringLiteral("ru.template"));
     application->setApplicationName(QStringLiteral("ContactList"));
 
-    ContactsDatabaseModel model;
-    ContactViewModel viewModel(&model);
-//    ContactModel abonentViewModel(&model);
-    AbonentCardViewModel abonentCardViewModel(&model);
+    DatabaseContacts database(&*application);
+
+    ContactViewModel viewModel(&database, &*application);
+    AbonentCardViewModel abonentCardViewModel(&database, &*application);
 
     QScopedPointer<QQuickView> view(Aurora::Application::createView());
 
     // NOTE: https://stackoverflow.com/questions/44168962/how-to-decide-where-the-sqlite-file-is-stored-in-qml-localstorage
-    view->engine()->setOfflineStoragePath(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation));
+//    view->engine()->setOfflineStoragePath(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation));
 
     view->rootContext()->setContextProperty("_viewModel", &viewModel);
-//    view->rootContext()->setContextProperty("_abonentViewModel", &abonentViewModel);
     view->rootContext()->setContextProperty("_abonentCardViewModel", &abonentCardViewModel);
     view->setSource(Aurora::Application::pathTo(QStringLiteral("qml/ContactList.qml")));
     view->show();
