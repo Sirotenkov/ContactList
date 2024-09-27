74c38e131c73e37b34a587147e2ae281107b80dd
Alex <aleksej.sirotenkov@simaster.ru>
2024-09-27 15:04:10 +0300

    Удален закомментированный код

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,6 @@ int main(int argc, char *argv[])
 
     QScopedPointer<QQuickView> view(Aurora::Application::createView());
 
-    // NOTE: https://stackoverflow.com/questions/44168962/how-to-decide-where-the-sqlite-file-is-stored-in-qml-localstorage
-//    view->engine()->setOfflineStoragePath(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation));
-
     view->rootContext()->setContextProperty("_viewModel", &viewModel);
     view->rootContext()->setContextProperty("_abonentCardViewModel", &abonentCardViewModel);
     view->setSource(Aurora::Application::pathTo(QStringLiteral("qml/ContactList.qml")));
